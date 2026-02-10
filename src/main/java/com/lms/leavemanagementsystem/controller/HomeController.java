e8eff1f05ae1475ab83bcf8cc7dcc1566f8165ba
MustafaGitZa <sibusisomst@gmail.com>
2026-02-10 13:45:15 +0200

    Fix home controller redirect

--- src/main/java/com/lms/leavemanagementsystem/controller/HomeController.java
+++ src/main/java/com/lms/leavemanagementsystem/controller/HomeController.java
@@ -6,15 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // Landing page
     @GetMapping("/")
     public String home() {
-        return "index";
-    }
-
-    // Login page
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+        return "redirect:/login";
     }
 }
